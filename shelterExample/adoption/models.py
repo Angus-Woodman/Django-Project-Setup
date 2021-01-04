cf6816c84fcdc6d3d2cebb83d0b5cbcfe7d95d57
Angus Woodman <>
2021-01-04 12:25:45 +0000

    initial SQlite database setup

--- shelterExample/adoption/models.py
+++ shelterExample/adoption/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+class Breed(models.Model):
+    name = models.CharField(max_length=100)
+    temperament = models.TextField()
+
+class Dog(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    breed = models.ForeignKey(Breed, null=True, on_delete=models.SET_NULL)
